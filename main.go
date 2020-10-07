6e7f70178718acf048d4f42cf8b9f2d8706fc329
Nitro <xiaoren8688@yahoo.co.jp>
2020-10-07 18:36:28 +0900

    conf_update

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"goImooc/go-gin-test/pkg/setting"
+	"log"
+)
 
+func main() {
+	log.Println("Hello, api 正在启动中...")
+	setting.SetUp()                             //初始化配置文件
+	log.Println(setting.ServerSetting.HttpPort) //测试能否打印出ini配置文件设置的信息
 }
